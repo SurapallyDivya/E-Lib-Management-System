83255b5a9f9beb08df03e55d7ba43c13c43c07a4
SurapallyDivya <divya.surapally2@gmail.com>
2023-04-07 18:11:29 +0530

    dao files added

---
+++ StudentDao.java
@@ -0,0 +1,61 @@
+package com.javatpoint.dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentDao
+ */
+@WebServlet("/StudentDao")
+public class StudentDao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public StudentDao() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	public static boolean authenticate(String stuId,String password){
+		boolean status=false;
+		try{
+			Connection con=DB.getCon();
+			PreparedStatement ps=con.prepareStatement("select * from student where studentId=? and password=?");
+			ps.setString(1,stuId);
+			ps.setString(2,password);
+			ResultSet rs=ps.executeQuery();
+			if(rs.next()){
+				status=true;
+			}
+			con.close();
+			
+		}catch(Exception e){System.out.println(e);}
+		
+		return status;
+	}
+
+}
